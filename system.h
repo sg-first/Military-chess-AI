1f34e7a456550f6ed31c17ed0d94ca0c62950731
3A <524019351@qq.com>
2019-06-15 23:31:43 +0800

    记录走法

--- system.h
+++ system.h
@@ -1,12 +1,34 @@
 #pragma once
 #include <array>
+#include <stack>
 #include "basic.h"
 #include "assess.h"
 using namespace std;
 
+class moveRecord
+{
+public:
+    moveRecord(int x1,int y1,int x2,int y2) : x1(x1),y1(y1),x2(x2),y2(y2),c1(cMap[y1][x1]),c2(cMap[y2][x2]) {}
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    char c1;
+    char c2;
+
+    void back()
+    {
+        cMap[y1][x1]=c1;
+        cMap[y2][x2]=c2;
+    }
+};
+
+stack<moveRecord>allRecord;
+
 //模拟移动棋子，返回结果（0己方被对方吃，1对方被己方吃，2对死，3己方移动）
 int simulateMove(int x1,int y1,int x2,int y2)
 {
+    allRecord.push(moveRecord(x1,y1,x2,y2)); //记录这步棋
     //移动的合法性由策略判别，这里直接进行实际的移动操作
     char &newPos=cMap[y2][x2];
     char &myChess=cMap[y1][x1];
